18559e97ee1a831b5723784b9e7d6965f34700f8
A-Max-01 <ztzsr13dz317@gmail.com>
2022-05-22 21:37:20 +0300

    create database

--- shorja_project/config/urls.py
+++ shorja_project/config/urls.py
@@ -17,9 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
-    path('', include("urls")),
     path('admin/', admin.site.urls),
+    path('', include("jumla.urls")),
 ]
 
 if settings.DEBUG:
--- shorja_project/jumla/models.py
+++ shorja_project/jumla/models.py
@@ -1,3 +1,76 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# المستخدم
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11)
+
+# الفئات
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Categories"
+
+# المحافظة
+
+
+class Governorate(models.Model):
+    name = models.CharField(max_length=100)
+
+# المحل
+
+
+class Shop(models.Model):
+    shopName = models.CharField(max_length=255)
+    shopOwner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shopOwner")
+
+# المنتجات
+
+
+class Product(models.Model):
+    ProductName = models.CharField(max_length=255)
+    Size = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.CharField(max_length=255)
+    shopOwner = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_products")
+    Date = models.DateTimeField(default=datetime.datetime.now())
+    Available = models.BooleanField(default=True)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product_Category")
+
+# لكل منتج اكثر من صورة
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to="images/")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="Image_Product")
+
+# سلة الزيون
+
+
+class Cart(models.Model):
+    userOwner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_cart")
+    Date = models.DateTimeField(default=datetime.datetime.now())
+    Governorate = models.ForeignKey(Governorate, on_delete=models.CASCADE, related_name="cart_Governorate")
+
+# الفاتورة
+
+
+class Bill(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="bill_cart")
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shop_bill")
+    products = models.ManyToManyField(Product, related_name="bill_products")
+    Date = models.DateTimeField(default=datetime.datetime.now())
+    quantity = models.IntegerField()
+    total = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+
